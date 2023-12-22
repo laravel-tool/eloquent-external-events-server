00ed7b2a63780b8aa1e376b1c320b6e3f055ecfe
Yuriy Potemkin <ysoft2000@mail.ru>
2023-12-22 19:11:28 +0300

    fix soft deletes events

--- src/EventService.php
+++ src/EventService.php
@@ -18,7 +18,11 @@ class EventService
         if (is_null($modelId) || in_array($event, ['deleted', 'forceDeleted'])) {
             $model = new $modelType;
         } else {
-            $model = $modelType::find($modelId);
+            if ($event == 'trashed') {
+                $model = $modelType::withTrashed()->find($modelId);
+            } else {
+                $model = $modelType::find($modelId);
+            }
         }
         /** @var Model $model */
         if (method_exists($model, 'fireEvent')) {
